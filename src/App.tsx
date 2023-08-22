6c282e20a8874e3916accb9df9cbd802ea5fb8c8
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-08-22 17:29:38 -0500

    Apply prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <div>
-      Hello World
-    </div>
-  )
+  return <div>Hello World</div>;
 }
 
-export default App
+export default App;
